7ad0d32d320ffcd1b2b5030ca15bd598bab85261
Mark Winterbottom <mark@winterbottom.me>
2015-09-15 14:59:48 +0100

    Added MariaDB and some other packages to the vagrant setup box.

--- vagrant_files/bootstrap.sh
+++ vagrant_files/bootstrap.sh
@@ -1,7 +1,23 @@
 #!/usr/bin/env bash
 
-sudo apt-get update
-#sudo apt-get -y upgrade
+# Update Packages
+sudo apt-get update && sudo apt-get -y upgrade
 
-sudo apt-get install -y python-pip
+# Install Useful/Required Packages
+sudo apt-get -y install python-pip python-dev libmysqlclient-dev git pv screen
+sudo apt-get -y install vim
 sudo pip install Django
+
+# Install MariaDB
+sudo apt-get -y install software-properties-common
+sudo apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 0xcbcb082a1bb943db
+sudo add-apt-repository 'deb http://mirror.stshosting.co.uk/mariadb/repo/10.0/debian jessie main'
+sudo debconf-set-selections <<< 'mariadb-server-10.0 mysql-server/root_password password rootpass'
+sudo debconf-set-selections <<< 'mariadb-server-10.0 mysql-server/root_password_again password rootpass'
+sudo apt-get update
+sudo apt-get install -y mariadb-server
+
+mysql -uroot -prootpass -e "CREATE DATABASE test_db;"
+
+# Install depdencies
+sudo pip install -r /vagrant/requirements.txt
